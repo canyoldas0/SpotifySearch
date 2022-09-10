a308223b2c8d4dba179e8867d02451c7613fe9a3
Can Yoldas <can.yoldas0@outlook.com>
2022-09-10 12:56:49 +0300

    detailvm tests added.

---
+++ SpotifyAPITests/SpotifyAPI/Modules/Detail/DetailViewModel.swift
@@ -0,0 +1,125 @@
+//
+//  DetailViewModel.swift
+//  SpotifyAPITests
+//
+//  Created by Can Yoldaş on 9.09.2022.
+//
+
+import Foundation
+import XCTest
+@testable import SpotifyAPI
+
+final class DetailViewModelTests: XCTestCase {
+    
+    private var sut: DetailViewModel!
+    private var mockDetail: MockDetailService!
+    private var authManager: MockAuthManager!
+    private var coordinatorDelegate: MockCoordinator!
+    private var viewDelegate: MockViewController!
+
+    
+    override func setUp() {
+        coordinatorDelegate = MockCoordinator()
+        let observationManager = ObservationManager()
+        
+        viewDelegate = MockViewController()
+        
+        mockDetail = MockDetailService()
+        authManager = MockAuthManager(observationManager: observationManager)
+        sut = DetailViewModel(
+            itemId: "Test",
+            detailService: mockDetail
+        )
+        sut.coordinatorDelegate = coordinatorDelegate
+        sut.delegate = viewDelegate
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        mockDetail = nil
+        coordinatorDelegate = nil
+        viewDelegate = nil
+        super.tearDown()
+    }
+}
+
+fileprivate final class MockViewController: DetailViewOutputProtocol {
+    
+    var outputs: [DetailViewOutput] = []
+    
+    func handleOutput(_ output: SpotifyAPI.DetailViewOutput) {
+        outputs.append(output)
+    }
+}
+
+fileprivate final class MockCoordinator: HomeCoordinatorDelegate {
+    
+    var goToLoginCalled = false
+    var goToProfileClicked = false
+    
+    func goBack(completion: SpotifyAPI.VoidHandler?) { }
+    
+    func goToLogin() {
+        goToLoginCalled = true
+    }
+    
+    func goToProfile() {
+        goToProfileClicked = true
+    }
+    
+    func goToDetail(animated: Bool, with id: String) { }
+    
+    func goToAuthScreen() { }
+    
+    func returnHome(completion: SpotifyAPI.VoidHandler?) { }
+    
+    func goToRoot(completion: SpotifyAPI.VoidHandler?) { }
+
+}
+
+fileprivate final class MockDetailService:  DetailServiceProtocol {
+    
+    func fetchDetailData(itemId: String, request: SpotifyAPI.Requestable, completion: @escaping (Result<SpotifyAPI.DetailResponse, Error>) -> Void) {
+        // TODO:
+    }
+    
+    func fetchArtistAlbumData(itemId: String, request: SpotifyAPI.Requestable, completion: @escaping (Result<SpotifyAPI.AlbumResponse, Error>) -> Void) {
+        // TODO:
+    }
+}
+
+fileprivate class MockAuthManager: AuthManagerProtocol {
+    
+    var observationManager: SpotifyAPI.ObservationManagerProtocol
+    
+    init(observationManager: SpotifyAPI.ObservationManagerProtocol) {
+        self.observationManager = observationManager
+    }
+    
+    func isSignedIn() -> Bool {
+        false
+    }
+    
+    func getSignInUrl() -> URL? {
+        nil
+    }
+    
+    func removeCacheIfNeeded() { }
+    
+    func logout() { }
+    
+    func exchangeCodeForToken(code: String, completion: @escaping SpotifyAPI.GenericHandler<Bool>) {
+        //
+    }
+    
+    func fetchData<T>(
+        endpoint: String,
+        httpMethod: SpotifyAPI.HTTPMethod,
+        requestable: SpotifyAPI.Requestable,
+        completion: @escaping ((Result<T, Error>) -> Void)) where T : Decodable {
+        //
+    }
+    
+    
+}
